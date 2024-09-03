fab4236c211790b88e28a743acbab8367eaabbfb
faiz877 <faiz74787@gmail.com>
2024-09-03 19:24:47 +0530

    updated about section

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -2,7 +2,13 @@ export const TechIcon = ({ component }: { component: React.ElementType }) => {
   const Component = component;
   return (
     <div>
-      <Component className="size-10"></Component>
+      <Component className="size-10 fill-[url(#tech-icon-gradient)]"></Component>
+      <svg className="size-0 absolute">
+        <linearGradient id="tech-icon-gradient">
+          <stop offset="0%" stopColor="rgb(110 231 183)" />
+          <stop offset="100%" stopColor="rgb(56 189 1248)" />
+        </linearGradient>
+      </svg>
     </div>
   );
 };
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -47,22 +47,44 @@ const hobbies = [
   {
     title: "Football",
     emoji: "⚽",
+    left: "5&",
+    top: "5&",
   },
   {
     title: "Film",
     emoji: "🎬",
+    left: "50&",
+    top: "5&",
   },
   {
     title: "Music",
     emoji: "🎧",
+    left: "10&",
+    top: "35&",
   },
   {
     title: "Reading",
     emoji: "📚",
+    left: "35%",
+    top: "40&",
   },
   {
     title: "Gaming",
     emoji: "🎮",
+    left: "70%",
+    top: "45%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "5%",
+    top: "65%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "45%",
+    top: "70%",
   },
 ];
 
@@ -75,7 +97,7 @@ export const AboutSection = () => {
           title="A Glimpse Into My World"
           description="Learn more about who I am, what I do and what inspires me"
         ></SectionHeader>
-        <div className="mt-20">
+        <div className="mt-20 flex flex-col gap-8">
           <Card className="h-[320px]">
             <CardHeader
               title="My Reads"
@@ -92,17 +114,31 @@ export const AboutSection = () => {
               className="px-6 pt-6"
             ></CardHeader>
             <ToolBoxItems items={toolboxItems} className="mt-6"></ToolBoxItems>
-            <ToolBoxItems items={toolboxItems} className="mt-6"></ToolBoxItems>
+            <ToolBoxItems
+              items={toolboxItems}
+              className="mt-6 "
+              itemsWrapperClassName="-translate-x-1/2"
+            ></ToolBoxItems>
           </Card>
-          <Card>
+          <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader
               title="Beyond The Code"
               description="Explore my interests and hobbies"
+              className="px-6 pt-6"
             ></CardHeader>
-            <div>
+            <div className="relative flex-1">
               {hobbies.map((hobby) => (
-                <div key={hobby.title}>
-                  <span>{hobby.title}</span>
+                <div
+                  key={hobby.title}
+                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{
+                    left: hobby.left,
+                    top: hobby.top,
+                  }}
+                >
+                  <span className="font-medium text-gray-950">
+                    {hobby.title}
+                  </span>
                   <span>{hobby.emoji}</span>
                 </div>
               ))}
